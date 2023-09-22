d801ec354952495afe433d431b638fb431e1eea2
RafaelAJustino <rafael.angelo.justino@gmail.com>
2023-09-22 17:22:03 -0300

    first commit

---
+++ src/templates/Volunteers/Volunteers.jsx
@@ -0,0 +1,13 @@
+import './Volunteers.css'
+import Image from 'next/image';
+
+
+function Volunteers() {
+    return (
+        <section id='volunteers'>
+            
+        </section>
+    )
+}
+
+export default Volunteers;
